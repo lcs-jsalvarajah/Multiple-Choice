c96e0b271ef1a401980e7638ebda6292efbf37f4
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2018-05-16 13:42:20 -0400

    DONE

--- MultipleChoice/main.swift
+++ MultipleChoice/main.swift
@@ -7,15 +7,39 @@ import Foundation
 
 // INPUT
 // Collect and filter user input here
-
+var expectedlines = 6
+var vaildInteger = 0
+while 1==1 {
+    // Test #1: Is it nil?
+    guard let givenString = readLine() else {
+        print("How many Lines?")
+        continue
+    }
+    // Test #2: Can it be an integer?
+    guard let givenInteger = Int(givenString) else {
+        print("How Many Lines?")
+        continue
+    }
+    // Test #3
+    if givenInteger <= 0  || givenInteger >= 10000 {
+        print("How Many Line?")
+        continue
+    }
+    // If all the tests passed
+    expectedlines = givenInteger * 2
+    break
+    
+}
 // PROCESS
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 
 // Example of how to collect multiple input lines
-let expectedLines = 4
-print("Please enter the \(expectedLines) of text:")
-for _ in 1...expectedLines {
+
+print("Please enter the \(expectedlines/2) student answers followed by the 3 lines of teacher answers")
+var letters: [String] = []
+var points = 0
+for _ in 1...expectedlines {
     
     // Get the input (use guard-let to guarantee it is not nil)
     // and then print it out
@@ -24,12 +48,29 @@ for _ in 1...expectedLines {
         continue
     }
     
+    
+    
     // Now we have the line, we can print it out, analyze it as needed, et cetera
-    print(givenLine)
+    letters.append(givenLine)
+    
+    
+    
+    
     
 }
 
+
 // OUTPUT
 // Report results to the user here
-
-
+if letters[0] == letters[3] {
+    points += 1
+}
+if letters[1] == letters[4] {
+    points += 1
+}
+if letters[2] == letters[5] {
+    points += 1
+} else {
+    print("No work")
+}
+print("You got \(points) right")
